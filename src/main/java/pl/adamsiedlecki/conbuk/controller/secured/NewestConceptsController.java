cd5307e4f5825e8d0ec57ee1fbf36f185b2579f7
adam <adamsiedlec@wp.pl>
2020-11-21 21:34:02 +0100

    I have added like mechanism to newest table
    Signed-off-by: adam <adamsiedlec@wp.pl>

--- src/main/java/pl/adamsiedlecki/conbuk/controller/secured/NewestConceptsController.java
+++ src/main/java/pl/adamsiedlecki/conbuk/controller/secured/NewestConceptsController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.adamsiedlecki.conbuk.db.concept.Concept;
 import pl.adamsiedlecki.conbuk.db.concept.ConceptService;
+import pl.adamsiedlecki.conbuk.tools.GetCurrentLoggedUser;
 
 import java.util.List;
 
@@ -23,6 +24,7 @@ public class NewestConceptsController {
     public String getRanking(Model model) {
         List<Concept> concepts = conceptService.getNewest();
         model.addAttribute("concepts", concepts);
+        model.addAttribute("loggedUser", GetCurrentLoggedUser.get());
         return "newestConcepts";
     }
 }
