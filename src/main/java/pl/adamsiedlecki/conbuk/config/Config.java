41e513e4dbac4e752223838aa5323a4ea6df5c7a
adam <adamsiedlec@wp.pl>
2020-12-03 15:44:57 +0100

    I have changed css styles for menu
    Signed-off-by: adam <adamsiedlec@wp.pl>

--- src/main/java/pl/adamsiedlecki/conbuk/config/Config.java
+++ src/main/java/pl/adamsiedlecki/conbuk/config/Config.java
@@ -3,4 +3,8 @@ package pl.adamsiedlecki.conbuk.config;
 public class Config {
 
     public static final String CELEBRATE_API_ADDRESS = "http://10.0.0.90/celebration";
+    
+    // this address is only for launching in school
+    //public static final String CELEBRATE_API_ADDRESS = "http://192.168.10.90/celebration";
+
 }
